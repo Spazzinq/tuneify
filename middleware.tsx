447e7fde720338e6a0e6f2c4ba647a7646aa7eb8
George <26337405+Spazzinq@users.noreply.github.com>
2023-10-24 23:31:40 -0400

    Update middleware.tsx

--- middleware.tsx
+++ middleware.tsx
@@ -4,12 +4,7 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
   // console.log(`Middleware called: ${req.url}`);
-  if (req.url === process.env.NEXTAUTH_URL) {
-    return NextResponse.next();
-  }
-
   try {
-    // try to get a token from the request
     const token = await getToken({ req });
     if (token) {
       return NextResponse.next();
@@ -22,15 +17,15 @@ export async function middleware(req: NextRequest) {
     }
   } catch (error) {
     // console.log("Error in middleware");
-    console.log(error);
+    // console.log(error);
     return new NextResponse(
       JSON.stringify({ success: false, message: error.message }),
       { status: 500, headers: { "content-type": "application/json" } }
     );
-  }
+  };
 }
  
 // See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: '/',
-// }
+export const config = {
+  matcher: '/protected/:path*',
+}
