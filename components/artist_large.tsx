19a33acc61e06626d8d3e3cee1a50e9fc08ef6c3
George <26337405+Spazzinq@users.noreply.github.com>
2023-11-06 20:35:22 -0500

    More attempts

--- components/artist_large.tsx
+++ components/artist_large.tsx
@@ -11,13 +11,11 @@ interface ArtistProps {
 
 const ArtistLarge: React.FC<ArtistProps> = ({ name, imageUrl, ranking, starRating }) => {
     return (
-        <div className="p-10">
-            <div className="flex flex-row mb-3 relative">
-                <div className="text-7xl font-bold absolute -left-5 -top-4">{ranking}</div>
-                <Image src={imageUrl} alt={name} width='200' height='200' className="rounded-lg aspect-square" />
-            </div>
-            <h2 className="text-left text-xl font-bold mb-1">{name}</h2>
-            <Rating starRating={starRating} />
+        <div className="relative">
+            <h3 className="text-7xl font-bold absolute -left-5 -top-4">{ranking}</h3>
+            <Image src={imageUrl} alt={name} width='200' height='200' className="rounded-lg aspect-square mb-3" />
+            <h3 className="text-left text-xl font-bold mb-1">{name}</h3>
+            {/* <Rating starRating={starRating} /> */}
         </div>
     );
 };
