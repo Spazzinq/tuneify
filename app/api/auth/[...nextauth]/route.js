078f015d333543488c81c6b91ab70b915bd282b3
George <26337405+Spazzinq@users.noreply.github.com>
2023-11-15 11:44:35 -0500

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-export const authOptions = {
+const authOptions = {
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
