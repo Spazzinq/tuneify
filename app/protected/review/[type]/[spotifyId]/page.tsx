1b46d6bdc748a4dc7f9a338de97f11fae8e3250c
George <26337405+Spazzinq@users.noreply.github.com>
2023-11-19 19:28:24 -0500

    Add hue

--- app/protected/review/[type]/[spotifyId]/page.tsx
+++ app/protected/review/[type]/[spotifyId]/page.tsx
@@ -21,6 +21,7 @@ export default async function Review({ params, searchParams }: { params: { spoti
             <textarea id="review" rows={8} className="block mb-8 p-2.5 w-full rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Write your thoughts here..."></textarea>
             <button type="submit" className="px-5 py-2.5 text-center text-white bg-gray-800 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-gray-900 hover:bg-gray-700">Publish</button>
         </form>
+        <footer className="mt-32 mb-10 text-center">Made with love by Team Tuneify.</footer>
       </main>
     );
   }
--- components/rating.tsx
+++ components/rating.tsx
@@ -22,7 +22,7 @@ const CustomRating: React.FC<RatingProps> = ({ spotifyId, type, starRating }) =>
 
     return (
         <div>
-            <Rating className='invert -left-1' name="simple-controlled"
+            <Rating className='hue-rotate-90 invert -left-1' name="simple-controlled"
                 value={value} precision={0.5} size="large"
                 onChange={changeRating}
             />
