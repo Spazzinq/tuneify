46c665d6bac48bf9eddb36218cb5f71cf0c40444
George <26337405+Spazzinq@users.noreply.github.com>
2023-11-19 19:54:21 -0500

    Fix search abr

--- components/logo.tsx
+++ components/logo.tsx
@@ -10,11 +10,12 @@ const russo = Russo_One({ subsets: ['latin'], weight: "400" })
 interface LogoProps {
     logoSize: number;
     fontSize: number;
+    className?: string;
 }
 
-const Logo: React.FC<LogoProps> = ({ logoSize, fontSize }) => {
+const Logo: React.FC<LogoProps> = ({ logoSize, fontSize, className }) => {
     return (
-        <div className="flex">
+        <div className={"flex " + className}>
             <Image src="/icon.svg" alt="Tuneify Logo" width={logoSize} height={logoSize} />
             <Link href="/" className={russo.className + " text-white text-" + fontSize + "xl font-bold ml-2 my-auto"}>TUNEIFY</Link>
         </div>
--- components/nav.tsx
+++ components/nav.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import LoginNav from './nav_login';
 import Logo from '@/components/logo';
 import { IconUnkown } from './icons';
-import Search from './search';
+import Searchbar from './search';
 import { Session } from 'next-auth';
 
 interface NavbarProps {
@@ -14,23 +14,21 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = async ({ session }) => {
     return (
         <nav className="bg-gray-800 mb-6">
-            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div className="relative flex items-center justify-between h-16">
-                    <Logo logoSize={40} fontSize={3} />
-                    <Search />
-                    <div className="hidden sm:block sm:ml-6">
-                        <div className="flex space-x-4">
-                            {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                            <Link href="/protected/profile" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                                Profile
-                            </Link>
-                            <Link href="/protected/diary" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                                Diary
-                            </Link>
-                        </div>
+            <div className="mx-auto max-w-8xl sm:px-6 lg:px-8">
+                <div className="flex flex-row items-center justify-center h-16 gap-5">
+                    <Logo logoSize={40} fontSize={3} className="mr-auto" />
+                    <Searchbar />
+                    <div className="flex">
+                        {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                        <Link href="/protected/profile" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                            Profile
+                        </Link>
+                        <Link href="/protected/diary" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                            Diary
+                        </Link>
                     </div>
-                    <div className="flex flex-row">
-                            <Image src={session?.user?.image || ''} alt="Profile Picture" placeholder='blur' blurDataURL='data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7' className="h-10 w-10 rounded-full mr-2" width={40} height={40} />
+                    <div className="flex flex-row ml-auto">
+                        <Image src={session?.user?.image || ''} alt="Profile Picture" placeholder='blur' blurDataURL='data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7' className="h-10 w-10 rounded-full mr-2" width={40} height={40} />
                         <LoginNav session={session}></LoginNav>
                         <button className="bg-white hover:bg-gray-100 text-gray-800 font-bold px-4 rounded">
                             + Review
