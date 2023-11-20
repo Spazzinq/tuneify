068ce5da08c799508461ef0cb88465b2a3871e49
George <26337405+Spazzinq@users.noreply.github.com>
2023-11-20 11:01:21 -0500

    Change icons

--- components/search_bar.tsx
+++ components/search_bar.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
 import { useRouter } from 'next/navigation';
 
 const Search = styled('div')(({ theme }) => ({
@@ -61,7 +60,9 @@ export default function Searchbar() {
     return (
         <Search onChange={search}>
             <SearchIconWrapper>
-                <SearchIcon />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" clipRule="evenodd" />
+                </svg>
             </SearchIconWrapper>
             <StyledInputBase
                 placeholder="Search…"
