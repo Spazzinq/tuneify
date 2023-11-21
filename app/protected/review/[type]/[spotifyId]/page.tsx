960c852e01b047d2413c15861f084d43de9a7aab
rachel <rsun1@swarthmore.edu>
2023-11-21 14:19:08 -0500

    added required attribute to textarea

--- app/protected/review/[type]/[spotifyId]/page.tsx
+++ app/protected/review/[type]/[spotifyId]/page.tsx
@@ -86,9 +86,9 @@ export default async function Review({ params, searchParams }: { params: { spoti
         <form action={create} className="flex flex-col mx-32 gap-5">
           {/* <input type="hidden" value={searchParams.rating} /> */}
           <label htmlFor="title" className="text-3xl">Title</label>
-          <textarea name="title" rows={1} className="block p-2.5 w-full rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Write your thoughts here..." defaultValue={review?.title}></textarea>
+          <textarea required name="title" rows={1} className="block p-2.5 w-full rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Write your thoughts here..." defaultValue={review?.title}></textarea>
           <label htmlFor="review" className="text-3xl">Review</label>
-          <textarea name="review" rows={8} className="block mb-8 p-2.5 w-full rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Write your thoughts here..." defaultValue={review?.content || ''}></textarea>
+          <textarea required name="review" rows={8} className="block mb-8 p-2.5 w-full rounded-lg border border-gray-300 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500" placeholder="Write your thoughts here..." defaultValue={review?.content || ''}></textarea>
           <button type="submit" className="px-5 py-2.5 text-center text-white bg-gray-800 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-gray-900 hover:bg-gray-700">Publish</button>
         </form>
       </main>
