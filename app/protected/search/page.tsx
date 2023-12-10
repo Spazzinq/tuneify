e6a57dc47e3f2e34654b35398838b9dc5522ac29
George <26337405+Spazzinq@users.noreply.github.com>
2023-12-09 22:47:45 -0500

    Add star fetch from search page

--- app/protected/search/page.tsx
+++ app/protected/search/page.tsx
@@ -3,6 +3,7 @@ import BoxTwoLine from "@/components/box_two_line";
 import { SpotifyArtist, SpotifyTrack, getSearchTop } from "@/spotify";
 import { redirect } from "next/navigation";
 import BoxGrid from "@/components/grid";
+import { getReviewFromCurrent } from "@/db";
 
 export default async function Page({ searchParams }: { searchParams: { [key: string]: string } }) {
     if (searchParams.query) {
@@ -31,14 +32,17 @@ async function formatTopQueries(title: string, query: string, type: string, limi
     let html = <></>;
 
     if (type === 'artist') {
-        html = data.artists.items.map((item: SpotifyArtist, index: Number) => {
-            return <BoxOneLine key={item.id} spotifyId={item.id} type="artist" title={item.name} imageUrl={item.images[1]?.url || ''} ranking={Number(index) + 1} starRating={0.01} />
+        html = data.artists.items.map(async (artist: SpotifyArtist, index: Number) => {
+            const reviewEntry = await getReviewFromCurrent(artist.id);
+
+            return <BoxOneLine key={artist.id} spotifyId={artist.id} type="artist" title={artist.name} imageUrl={artist.images[1]?.url || ''} ranking={Number(index) + 1} starRating={reviewEntry ? reviewEntry.stars : 0.01} />
         });
     } else if (type === 'track') {
-        html = data.tracks.items.map((track: SpotifyTrack, index: Number) => {
-            let album = track.album
+        html = data.tracks.items.map(async (track: SpotifyTrack, index: Number) => {
+            const reviewEntry = await getReviewFromCurrent(track.id);
+            const album = track.album
 
-            return <BoxTwoLine key={track.id} spotifyId={track.id} type="track" title={track.name} subtitle={album.name} imageUrl={album.images[1]?.url || ''} ranking={Number(index) + 1} starRating={0.01} />
+            return <BoxTwoLine key={track.id} spotifyId={track.id} type="track" title={track.name} subtitle={album.name} imageUrl={album.images[1]?.url || ''} ranking={Number(index) + 1} starRating={reviewEntry ? reviewEntry.stars : 0.01} />
         });
     }
 
